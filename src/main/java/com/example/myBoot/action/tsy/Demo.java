10009155b02cb7bd5e73c86fba1d246f256785f5
tsy2422713476 <tsy2422713476@163.com>
2019-01-04 10:56:27 +0800

    第一次git项目提交

---
+++ src/main/java/com/example/myBoot/action/tsy/Demo.java
@@ -0,0 +1,12 @@
+package com.example.myBoot.action.tsy;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tan哈哈0...");
+		System.out.println("Tan哈哈1...");
+		System.out.println("Tan哈哈2...");
+	}
+
+}
